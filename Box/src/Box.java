ad3651256dee647fc6d07ebe44663bd8578ace11
BenjaminISS <benjamin.chauviere@sk.ibm.com>
2017-03-13 18:19:18 +0100

    Initial commitment

---
+++ Box/src/Box.java
@@ -0,0 +1,33 @@
+
+public class Box {
+
+	/**
+	 * @param args
+	 */
+	int width;
+	int height;
+	int length;
+	
+	public int Volume(int w, int h, int l) {
+		
+		return w*h*l;
+	}
+	
+	//Main function
+	public static void main(String[] args) {
+		
+		Box b = new Box();
+		b.width=2;
+		b.height=2;
+		b.length=3;
+		
+		System.out.println("Hello Box "+b.Volume(2,2,3));
+
+		
+		int a1 = 1;
+		int a2= 19;
+		if (a1 == a2)
+			System.out.println("Equality");
+	}
+
+}
